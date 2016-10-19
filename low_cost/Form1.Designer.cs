d05ff6dbb39c81a9b5a8eb6176cff714efa4c257
Marijo Horvat <Marijo.Horvat@vsite.hr>
2016-10-19 16:10:47 +0200

    Add project files.

---
+++ low_cost/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace low_cost
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(304, 231);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(304, 167);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(229, 24);
+            this.comboBox1.TabIndex = 1;
+            comboBox1Load();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(304, 299);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(229, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 629);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        public void comboBox1Load()
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            string sql = null;
+            connetionString = low_cost.Properties.Settings.Default.iataConnectionString;
+            sql = "select airport AS name from iata_airport_codes";
+            connection = new SqlConnection(connetionString);
+            connection.Open();
+            command = new SqlCommand(sql, connection);
+            adapter.SelectCommand = command;
+            adapter.Fill(ds);
+            adapter.Dispose();
+            command.Dispose();
+            connection.Close();
+            comboBox1.DataSource = ds.Tables[0];
+            comboBox1.ValueMember = "airport";
+            comboBox1.DisplayMember = "name";
+        }
+    }
+    
+}
+
---
+++ low_cost/airport.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace low_cost
+{
+    /// <summary>
+    /// Origin and Destination data
+    /// </summary>
+    class Airport
+    {
+        protected string airportName;
+        protected string iata;
+        protected DateTime time;
+        protected uint passengers;
+        protected string currency;
+        protected uint id; 
+
+        public void SetData(string name, DateTime t, uint pass, string curr,uint ident)
+        {
+            airportName = name;
+            time = t;
+            passengers = pass;
+            currency = curr;
+            iata = SetIata(airportName);
+            id = ident;
+        }
+        public string Name
+        {
+            get { return airportName; }
+            set { airportName = value; }
+        }
+        public string AirportIata
+        {
+            get { return iata; }
+            set { iata = SetIata(value); }
+        }
+        public DateTime Date
+        {
+            get { return time; }
+            set { time = value; }
+        }
+        public uint PassengerCount
+        {
+            get { return passengers; }
+            set { passengers = value; }
+        }
+        public string CurrencyUsed
+        {
+            get { return currency; }
+            set { currency = value; }
+        }
+        public uint QueryId
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        // Convert airfield name into IATA code
+        protected string SetIata(string airportName)
+        {
+            string connectionString = low_cost.Properties.Settings.Default.iataConnectionString;
+            SqlConnection connect = new SqlConnection(connectionString);
+            SqlCommand query = new SqlCommand("SELECT code FROM iata_airport_codes WHERE airport = " + airportName, connect);
+            SqlDataReader reader = query.ExecuteReader();
+            string iata = reader.GetString(reader.GetOrdinal("code"));
+            return iata;
+        }
+    }
+    /// <summary>
+    /// Creating of an URL string for Amadeus
+    /// </summary>
+    class CreateUrl
+        {
+            protected string url = "http://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?";
+
+            protected void setString(Airport origin, Airport destination)
+            {
+                string apikey = "apikey=YQGzMuqt1UQXZBrmRAZO250tS1j1sFBQ" + "&";
+                string ori = "origin=" + origin.AirportIata + "&";
+                string des = "destination=" + destination.AirportIata + "&";
+                string tim = "departure_date=" + origin.Date + "&";
+                string pas = "adults=" + origin.PassengerCount + "&";
+                string sur = "¤cy=" + origin.CurrencyUsed + " HTTP / 1.1";
+                url = url + apikey + ori + des + tim + pas + sur;
+            }
+            public string GetData()
+            {
+                return url;
+            }
+        }
+    /// <summary>
+    /// Compare current query with local database querries
+    /// </summary>
+    class CompareQuery : CreateUrl
+            {
+        protected string connectionString = low_cost.Properties.Settings.Default.iataConnectionString;
+        public bool CompareData(Airport origin, Airport destination)
+        {    
+            SqlConnection connect = new SqlConnection(connectionString);
+            SqlCommand query = new SqlCommand("SELECT Origin, Destination, DateOfTravel, Passengers, Currency FROM QuerryTable", connect);
+            SqlDataReader reader = query.ExecuteReader();
+            if (reader.HasRows)
+            {
+                if (reader.GetString(reader.GetOrdinal("Origin")) != origin.AirportIata)
+                    return false;
+                if (reader.GetString(reader.GetOrdinal("Destination")) != destination.AirportIata)
+                    return false;
+                if (DateTime.Compare(origin.Date, reader.GetDateTime(reader.GetOrdinal("DateOfTravel"))) != 0)
+                    return false;
+                if (reader.GetInt32(reader.GetOrdinal("Passengers")) != origin.PassengerCount)
+                    return false;
+                if (reader.GetString(reader.GetOrdinal("Currency")) != origin.CurrencyUsed)
+                    return false;
+            }
+            else
+                return false;
+            return true;
+        }
+            }
+    /// <summary>
+    /// Save query to database. 
+    /// Save results from Amadeus to database
+    /// </summary>
+    class SaveData : CompareQuery
+    {
+        public void SaveQuerylData(Airport origin, Airport destination)
+        {
+            SqlConnection connect = new SqlConnection(connectionString);
+            SqlCommand query = new SqlCommand("INSERT INTO QueryTable (Origin, Destination, DateOfTravel, Passengers, Currency, ID) VALUES (@Origin, @Destination, @DateOfTravel, @Passengers, @Currency, @ID)");
+            query.CommandType = CommandType.Text;
+            query.Connection = connect;
+            query.Parameters.AddWithValue("@Origin", origin.AirportIata);
+            query.Parameters.AddWithValue("@Destination", destination.AirportIata);
+            query.Parameters.AddWithValue("@DateOfTravel", origin.Date);
+            query.Parameters.AddWithValue("@Passengers", origin.PassengerCount);
+            query.Parameters.AddWithValue("@Currency", origin.CurrencyUsed);
+            query.Parameters.AddWithValue("@ID", origin.QueryId);
+            connect.Open();
+            query.ExecuteNonQuery();
+        }
+        public void SaveResultData()
+        {
+            // TODO
+            // data collected from Amadeus saved to a local db
+            // data saved with the same id as query
+        }
+    }
+    /// <summary>
+    /// Send query to Amadeus, receive query result from Amadeus
+    /// </summary>
+    class SendHttpRequest : CreateUrl
+    {
+        protected string html = string.Empty;
+        public void send(Airport origin, Airport destination)
+        {
+            setString(origin, destination);
+
+            string html = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+        }
+    }
+    /// <summary>
+    /// Parsing of collected result into relevat data
+    /// </summary>
+    class ParseData : SendHttpRequest
+    {
+        //parse relevant data
+        // transform json data into c# class data
+    }
+    /// <summary>
+    /// Displays the data
+    /// </summary>
+    class DisplayData
+    {
+        //  TODO
+        //     Display data from local db
+        //     concept: get id from query, get saved data by id, display it
+    }
+}
